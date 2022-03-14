50f97a1387dadcd1e97f2eba7a55aec5ad3da62e
unknown <guptat320@gmail.com>
2022-03-14 13:31:36 +0530

    deploying

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,6 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/", UserRouter);
 app.use("/", HouseRouter);
 
-const port = process.env.PORT
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
-app.listen(port || 5000, () => {
+app.listen(5000, () => {
   console.log("server is running on port 3000");
 });
